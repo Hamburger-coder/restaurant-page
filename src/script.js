2f8139fc40e92752f76aaf0df4dd51a577c61acb
Hamburger-coder <joesmast@gmail.com>
2024-11-12 17:44:40 -0500

    Add webpack.config.js for ininitial configuration, Create basic HTML skeleton, Install webpack-dev-server for live reloading

---
+++ src/script.js
@@ -0,0 +1,8 @@
+// index.js
+import "./styles.css";
+// import odinImage from "./images/odin.png";
+   
+// const image = document.createElement("img");
+// image.src = odinImage;
+   
+// document.body.appendChild(image);
