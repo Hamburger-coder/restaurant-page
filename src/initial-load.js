0c766600aae79fe392722ed3d9dc4535a3843484
Hamburger-coder <joesmast@gmail.com>
2024-11-18 17:48:36 -0500

    Remove content from the #content div and reenters it with JS

--- src/initial-load.js
+++ src/initial-load.js
@@ -1,8 +1,11 @@
-# restaurant-page
-Restaurant page for The Odin Project
+let container = document.getElementById("content")
 
-        <h1>Sabor del Sol</h1>
+
+export function loadPage() {
+    let innerContent = `        <h1>Sabor del Sol</h1>
         <img class="pizza-img" src="https://images.pexels.com/photos/8230019/pexels-photo-8230019.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Imagen de comida" height="500">
         <p>Sabor del Sol is open from 11 AM to 10 PM, Monday through Saturday</p>
         <p>Experience the vibrant flavors of Mexico at Sabor del Sol, where every dish is a celebration of sunshine, tradition, and authentic culinary excellence!</p>
-        
+        `
+    container.innerHTML = innerContent; 
+}
--- src/script.js
+++ src/script.js
@@ -1,8 +1,10 @@
 // index.js
 import "./styles.css";
+import { loadPage } from "./initial-load";
 // import odinImage from "./images/odin.png";
    
 // const image = document.createElement("img");
 // image.src = odinImage;
    
 // document.body.appendChild(image);
+loadPage();
