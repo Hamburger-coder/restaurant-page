c06c9faec6bb53286154a57b81ba4fda49a22bd3
Hamburger-coder <joesmast@gmail.com>
2024-11-20 17:13:38 -0500

    Add styling for buttons

--- src/initial-load.js
+++ src/initial-load.js
@@ -5,7 +5,7 @@ export function loadPage() {
     let innerContent = `        <h1>Sabor del Sol</h1>
         <img class="pizza-img" src="https://images.pexels.com/photos/8230019/pexels-photo-8230019.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" alt="Imagen de comida" height="500">
         <p>Sabor del Sol is open from 11 AM to 10 PM, Monday through Saturday</p>
-        <p>Experience the vibrant flavors of Mexico at Sabor del Sol, where every dish is a celebration of sunshine, tradition, and authentic culinary excellence!</p>
+        <p>Check out Sabor del Sol for some awesome Mexican flavors! Every dish is all about sunshine, tradition, and great food.</p>
         `
     container.innerHTML = innerContent; 
 }
